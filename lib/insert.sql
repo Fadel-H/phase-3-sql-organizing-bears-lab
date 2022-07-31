cf79dd457cf7bafb89b63fb8acd5282238a0824b
Fadel <fadel.g.heji@gmail.com>
2022-07-31 15:49:27 -0400

    commit tree 7500901b441198b60c4ef0a41d70c2e4f29cf384

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Mr. Chocolate", 12, "M", "brown", "reliable", 1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Rowdy", 22, "M", "orange", "daring", 1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Tabitha", 26, "F", "green", "honest", 1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Sergeant Brown", 7, "M", "blue", "tough", 0);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Melissa", 30, "F", "yellow", "imaginative", 0);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Grinch", 40, "M", "green", "mean", 1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES ("Wendy", 20, "F", "red", "charming", 1);
+
+INSERT INTO bears (name,age,sex,color,temperament,alive) VALUES (NULL, 100, "M", "white", "bizarre", 1);
